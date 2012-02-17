9ddb530a67405fef9ff8c7c2a083c93ade1eba7c
James McKinney <james@slashpoundbang.com>
2012-02-17 14:29:23 -0500

    recover concordances even if centroid missing. set default province

--- postcodes/models.py
+++ postcodes/models.py
@@ -18,8 +18,39 @@ class Postcode(models.Model):
 
     objects = models.GeoManager()
 
+    # @see http://en.wikipedia.org/wiki/List_of_postal_codes_in_Canada List of postal codes in Canada
     def save(self, *args, **kwargs):
         self.clean_fields()
+        if not self.province:
+            if self.code[0] == 'X'
+                self.province = {
+                    'X0A': 'NU',
+                    'X0B': 'NU',
+                    'X0C': 'NU',
+                    'X0E': 'NT',
+                    'X0G': 'NT',
+                    'X1A': 'NT',
+                }.get(self.code[0:3])
+            else:
+                self.province = {
+                    'A': 'NL',
+                    'B': 'NS',
+                    'C': 'PE',
+                    'E': 'NB',
+                    'G': 'QC',
+                    'H': 'QC',
+                    'J': 'QC',
+                    'K': 'ON',
+                    'L': 'ON',
+                    'M': 'ON',
+                    'N': 'ON',
+                    'P': 'ON',
+                    'R': 'MB',
+                    'S': 'SK',
+                    'T': 'AB',
+                    'V': 'BC',
+                    'Y': 'YT',
+                }.get(self.code[0])
         super(Postcode, self).save(*args, **kwargs)
 
     def __unicode__(self):
@@ -31,21 +62,21 @@ class Postcode(models.Model):
             'city': self.city,
             'province': self.province
         }
+        concordances = PostcodeConcordance.objects.filter(code=self.code).values_list('boundary', flat=True)
+        concordance_sets = set()
+        if concordances:
+            q = ( (models.Q(set=c.split('/')[0]) & models.Q(slug=c.split('/')[1])) for c in concordances )
+            boundaries = Boundary.objects.filter( reduce(lambda a,b: a | b, q) )
+            boundaries = Boundary.prepare_queryset_for_get_dicts(boundaries)
+            boundaries = Boundary.get_dicts(boundaries)
+            for b in boundaries:
+                concordance_sets.add(b['boundary_set_name'])
+            r['boundaries_concordance'] = boundaries
         if self.centroid:
             r['centroid'] = {
                'type': 'Point',
                'coordinates': [self.centroid.x, self.centroid.y]
             }
-            concordances = PostcodeConcordance.objects.filter(code=self.code).values_list('boundary', flat=True)
-            concordance_sets = set()
-            if concordances:
-                q = ( (models.Q(set=c.split('/')[0]) & models.Q(slug=c.split('/')[1])) for c in concordances )
-                boundaries = Boundary.objects.filter( reduce(lambda a,b: a | b, q) )
-                boundaries = Boundary.prepare_queryset_for_get_dicts(boundaries)
-                boundaries = Boundary.get_dicts(boundaries)
-                for b in boundaries:
-                    concordance_sets.add(b['boundary_set_name'])
-                r['boundaries_concordance'] = boundaries
             boundaries = Boundary.objects.filter(shape__contains=self.centroid)
             boundaries = Boundary.prepare_queryset_for_get_dicts(boundaries)
             r['boundaries_centroid'] = filter(
