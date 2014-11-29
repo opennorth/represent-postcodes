a712fe88b261e0e47c620cea8d0b037f935f071b
James McKinney <james@slashpoundbang.com>
2014-11-29 12:18:25 -0500

    add description, url to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name="represent-postcodes",
     version="0.0.1",
+    description="A web API for postal codes associated to electoral districts, packaged as a Django app.",
+    url="https://github.com/rhymeswithcycle/represent-postcodes",
     license="MIT",
     packages=[
         'postcodes',
