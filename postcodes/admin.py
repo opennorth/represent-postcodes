bb6bc11b2f554560b63ba3f77161b3cd8aa3a718
James McKinney <james@slashpoundbang.com>
2017-02-04 23:27:31 -0500

    flake8

--- postcodes/admin.py
+++ postcodes/admin.py
@@ -6,10 +6,12 @@ from postcodes.models import Postcode, PostcodeConcordance
 class PostcodeAdmin(admin.OSMGeoAdmin):
     list_display = ('code', 'city', 'province')
 
+
 admin.site.register(Postcode, PostcodeAdmin)
 
 
 class PostcodeConcordanceAdmin(admin.ModelAdmin):
     list_display = ('code', 'boundary', 'source')
+
 
 admin.site.register(PostcodeConcordance, PostcodeConcordanceAdmin)
