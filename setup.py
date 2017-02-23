90b582216240aeadbb05b57b6dba041d7e1db325
James McKinney <james@slashpoundbang.com>
2017-02-23 11:16:30 -0500

    Fix setup.py url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="represent-postcodes",
     version="0.0.1",
     description="A web API for postal codes associated to electoral districts, packaged as a Django app.",
-    url="https://github.com/rhymeswithcycle/represent-postcodes",
+    url="https://github.com/opennorth/represent-postcodes",
     license="MIT",
     packages=[
         'postcodes',
