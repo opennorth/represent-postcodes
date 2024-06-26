1dca8a647b57b4954f6ec8b0c2ddde33754a2e1f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-06-26 14:59:06 -0400

    chore: Use consistent order with represent-boundaries

--- postcodes/admin.py
+++ postcodes/admin.py
@@ -3,11 +3,11 @@ from django.contrib.gis import admin
 from postcodes.models import Postcode, PostcodeConcordance
 
 
-@admin.register(Postcode)
-class PostcodeAdmin(admin.OSMGeoAdmin):
-    list_display = ('code', 'city', 'province')
-
-
 @admin.register(PostcodeConcordance)
 class PostcodeConcordanceAdmin(admin.ModelAdmin):
     list_display = ('code', 'boundary', 'source')
+
+
+@admin.register(Postcode)
+class PostcodeAdmin(admin.OSMGeoAdmin):
+    list_display = ('code', 'city', 'province')
